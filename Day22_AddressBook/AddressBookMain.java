b73235c4b56b460ff2eff1b38d6970a96630c7dd
PoojaSolankar <poojasolankar09gmail.com>
2022-04-03 20:46:15 +0530

    Address Book

---
+++ Day22_AddressBook/AddressBookMain.java
@@ -0,0 +1,16 @@
+package com.Bridgelabz.Day22_AddressBook;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+	public static Scanner scannerObject = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome To Address Book Program!!");
+		AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
+		addressBookDirectory.operationDirectory();
+
+	}
+
+
+}
